fa41a19d4b39f1941ae63db51521023341a51ddc
Priyanshu2004-Singh <priyanshu.23scse1011413@galgotiasuniversity.ac.in>
2025-10-29 02:30:18 +0530

    added multer for file uploads

--- backend/src/controllers/addFood.controller.js
+++ backend/src/controllers/addFood.controller.js
@@ -1,7 +1,14 @@
 import authFoodPartnerMiddleware from "../../middleware/auth.middleware.js";
 import FoodItem from "../modal/foodItem.modal.js";
 
+
 export const createFood = async (req, res) => {
-    console.log(req.foodPartner); // return undefined
+    // console.log(req.foodPartner); // return undefined
+    //!  Using multer we can access form data in req.body video
+
+    console.log(req.body);
+    // chekiing file
+    console.log(req.file); // to access video file
     res.json({ message: "Food item added successfully" });
 };
+
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 import {createFood } from "../controllers/addFood.controller.js";
 import authFoodPartnerMiddleware from "../../middleware/auth.middleware.js";
 const router = Router();
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // ! Adding Food Item Controller  :- It is protected route , only food partners can add food items
-router.post('/addFoodItem', authFoodPartnerMiddleware, createFood);
+router.post('/addFoodItem', authFoodPartnerMiddleware, upload.single('video'), createFood);
 
 export default router;
